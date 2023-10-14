65f82626ab1ab35a55bc70cc9fdd98d35218e63f
AnmZ <anmit.ray911@gmail.com>
2023-10-14 20:08:19 +0530

    Simple card dealing using fp (Elixir)

---
+++ lib/cards.ex
@@ -0,0 +1,57 @@
+defmodule Cards do
+
+  @moduledoc """
+    provides methods for creating and handling a deck iof cards
+  """
+
+  @doc """
+    Interface to create deck of playing cards
+  """
+  def create_deck do
+    values =  ["Ace","Two","Three","Four","Five","King"]
+    suits = ["Spades","Clubs","Hearts","Diamonds"]
+      for suit <-suits,value <-values do
+        "#{value} of #{suit}"
+      end
+  end
+@doc """
+  Randomly shuffles your deck
+"""
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+@doc """
+  checks if a card is present in a deck
+"""
+  def contains?(deck,card) do
+    Enum.member?(deck,card)
+  end
+@doc """
+    Deal or distribute a card from deck with hand_size
+"""
+  def deal(deck,hand_size) do
+    Enum.split(deck,hand_size)
+  end
+@doc """
+    Saves deck to file storage
+"""
+  def save(deck,fileName) do
+      binary = :erlang.term_to_binary(deck);
+      File.write(fileName,binary);
+  end
+@doc """
+  Loads deck from file storage
+"""
+  def load(filename) do
+      case File.read(filename) do
+          {:ok,binary} -> :erlang.binary_to_term binary
+          {:error,reason} -> "File doesnot exists,Something went wrong,#{reason}"
+      end
+  end
+
+  def create_hand(handSize)do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(handSize)
+  end
+end
